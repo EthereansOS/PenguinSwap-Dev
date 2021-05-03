8ee54d099ec25981416bb570244beddd8f88cf5f
Marco Vasapollo <ceo@metaring.com>
2021-05-03 20:39:22 +0200

    Full redirect

--- src/pages/Item/createItemWizard/innerLoader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import createReactClass from 'create-react-class'
-
-export default createReactClass({
-    render() {
-        return (<span>Loading...</span>);
-    }
-});
--- src/pages/Item/index.tsx
+++ src/pages/Item/index.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import AppBody from "pages/AppBody";
 import { Wrapper } from "components/swap/styleds";
 import { Button } from "theme/components";
-import { getPenguinCollectionAddress, getPenguinCollectionWrapLink, loadItemCollections } from "utils/itemIntegration";
+import { getPenguinCollectionWrapLink, loadItemCollections, getPenguinCollectionCreateLink } from "utils/itemIntegration";
 
-import CreateItemWizard from './createItemWizard/view'
 import { useActiveWeb3React } from 'hooks'
 
 export default function Item() {
 
-  const [createItem, setCreateItem] = useState(false);
 
   const { library, chainId } = useActiveWeb3React();
 
@@ -21,18 +19,14 @@ export default function Item() {
   return (
     <AppBody>
       <Wrapper id="item-page">
-        {!createItem && <>
+        <>
           <Button onClick={() => window.open(getPenguinCollectionWrapLink(), '_blank')}>
             Wrap An Item
           </Button>
-          <Button onClick={() => setCreateItem(true)}>
+          <Button onClick={() => window.open(getPenguinCollectionCreateLink(chainId), '_blank')}>
             Create a new Item
           </Button>
-        </>}
-        {createItem && <>
-          <h3>Create Item</h3>
-          <CreateItemWizard collectionAddress={getPenguinCollectionAddress(chainId)} onCancel={() => setCreateItem(false)}/>
-        </>}
+        </>
       </Wrapper>
     </AppBody>
   )
